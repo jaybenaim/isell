c4b9cef8a1bb4b06fbea3e9ca5c69b04a770e3b6
jaybenaim <benaimjacob@gmail.com>
2020-01-09 19:58:32 -0500

    invalid hostheader

--- src/server.js
+++ src/server.js
@@ -53,11 +53,12 @@ var corsOptionsDelegate = function(req, callback) {
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
 
-app.use(express.static(path.join(__dirname, 'build')));
--app.get('/', function (req, res) {
-+app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+app.use(express.static(path.join(__dirname, "build")));
+-app.get("/", function(req, res) {
+  +app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+});
 app.options("/charge", cors(corsOptionsDelegate));
 app.post("/charge", cors(corsOptionsDelegate), async (req, res, next) => {
   try {
