389bf43eade45c16c7fb463c3b67d6418fd642af
jaybenaim <benaimjacob@gmail.com>
2020-01-10 16:05:37 -0500

    removed webpack.config require error crashing app

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const path = require("path");
-const options = {
-  protocol: window.location.protocol === "https:" ? "wss" : "wss"
-};
-module.exports = {
-  devtool: false,
-  plugins: [new webpack.EvalSourceMapDevToolPlugin(options)]
-};
+// const path = require("path");
+// const options = {
+//   protocol: window.location.protocol === "https:" ? "wss" : "wss"
+// };
+// module.exports = {
+//   devtool: false,
+//   plugins: [new webpack.EvalSourceMapDevToolPlugin(options)]
+// };
