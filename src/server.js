927f03deb5656c2263a91238d201b7fd5108ad32
jaybenaim <benaimjacob@gmail.com>
2020-01-09 19:39:57 -0500

    host .env

--- src/server.js
+++ src/server.js
@@ -47,11 +47,17 @@ var corsOptionsDelegate = function(req, callback) {
 // app.get("/", (req, res) => {
 //   res.write("<h1>Home</h1>");
 // });
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
+// app.use(express.static(path.join(__dirname, "build")));
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+app.use(express.static(path.join(__dirname, 'build')));
+-app.get('/', function (req, res) {
++app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 app.options("/charge", cors(corsOptionsDelegate));
 app.post("/charge", cors(corsOptionsDelegate), async (req, res, next) => {
   try {
