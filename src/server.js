bc06e62a77d38810801b4fab9e9985bfa79c6ce7
jaybenaim <benaimjacob@gmail.com>
2020-01-10 23:32:58 -0500

    server updated cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://jaybenaim.github.io"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
