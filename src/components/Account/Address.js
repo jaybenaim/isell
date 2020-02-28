959d8d145da02c7f3f0d9e470103c5213696c8f8
jaybenaim <benaimjacob@gmail.com>
2020-02-27 21:43:42 -0500

    added form to modal

--- src/components/Account/Address.js
+++ src/components/Account/Address.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import backend from "../../Api/backend";
-import { Modal, Button } from "react-bootstrap";
+import AddressForm from "./AddressForm";
 import "./address.css";
 class Address extends Component {
   state = {
@@ -32,32 +32,10 @@ class Address extends Component {
           Add an address
         </button>
 
-        <div>
-          <Modal
-            show={showForm}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-          >
-            <Modal.Header>
-              <Modal.Title id="contained-modal-title-vcenter">
-                Modal heading
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h4>Centered Modal</h4>
-              <p>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo
-                odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                risus, porta ac consectetur ac, vestibulum at eros.
-              </p>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={() => this.showAddressForm()}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
-
+        <AddressForm
+          showForm={showForm}
+          showAddressForm={this.showAddressForm}
+        />
         <a href="https://icons8.com/icon/1501/plus"></a>
       </div>
     );
---
+++ src/components/Account/AddressForm.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+
+class AddressForm extends Component {
+  state = {};
+  render() {
+    const { showForm, showAddressForm } = this.props;
+    return (
+      <div>
+        <Modal
+          show={showForm}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Modal heading
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => showAddressForm()}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default AddressForm;
