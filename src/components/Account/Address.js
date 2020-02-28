2e61e27ee56b6f5ed9e75882962d284c2cdad868
jaybenaim <benaimjacob@gmail.com>
2020-02-27 21:27:32 -0500

    added modal for add adress form

--- src/components/Account/Address.js
+++ src/components/Account/Address.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import backend from "../../Api/backend";
+import { Modal, Button } from "react-bootstrap";
 import "./address.css";
 class Address extends Component {
-  state = {};
+  state = {
+    showForm: false
+  };
+  showAddressForm = () => {
+    const { showForm } = this.state;
+    this.setState({ showForm: !showForm });
+  };
   handleAddAddress = () => {
     // TODO call api/profiles and post shippingInfo
-    return;
+    this.showAddressForm();
   };
   render() {
+    const { showForm } = this.state;
+
     return (
       <div>
         <h4>Shipping Address</h4>
@@ -21,6 +31,33 @@ class Address extends Component {
           />
           Add an address
         </button>
+
+        <div>
+          <Modal
+            show={showForm}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Modal heading
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>Centered Modal</h4>
+              <p>
+                Cras mattis consectetur purus sit amet fermentum. Cras justo
+                odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
+                risus, porta ac consectetur ac, vestibulum at eros.
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => this.showAddressForm()}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+
         <a href="https://icons8.com/icon/1501/plus"></a>
       </div>
     );
